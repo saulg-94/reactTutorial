9c755575e2ddfad86748a799bb1ceef8b7ac3cf7
Saul Gonzalez <94.gonzlezsaul@gmail.com>
2022-09-27 15:48:27 -0400

    beginning lecture videos and taking notes had to delete alot of the boilerplate files

--- react-tutorial/src/index.js
+++ react-tutorial/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// CSS
+
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL600_SR600,400_.jpg"
+    alt=""
+  />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+
+const Author = () => <h4>Amelia Hepworth</h4>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
