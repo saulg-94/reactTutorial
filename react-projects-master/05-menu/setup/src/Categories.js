a51757e5d4f63ccbbec186f4c4f4c64ed22d6f5a
Saul Gonzalez <94.gonzlezsaul@gmail.com>
2022-10-13 12:59:47 -0400

    finished Menu project

--- react-projects-master/05-menu/setup/src/Categories.js
+++ react-projects-master/05-menu/setup/src/Categories.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return (
+          <button
+            type="button"
+            className="filter-btn"
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
+// manually setup each button to filter thru each category // or use the method show above
+
+// return (
+//   <div className="btn-container">
+//     <button className="filter-btn" onClick={() => filterItems("breakfast")}>
+//       breakfast
+//     </button>
+//     <button className="filter-btn" onClick={() => filterItems("all")}>
+//       all
+//     </button>
+//   </div>
+// );
 
 export default Categories;
